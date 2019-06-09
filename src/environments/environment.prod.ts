0efda6b767e57bd270a96d79faf132c8a520cade
Juliet <chepngetichjuliet@gmail.com>
2019-06-09 19:53:27 +0300

    adds apis to the environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: false,
+  apiGithub: "https://api.github.com/users/Julzpeter?access_token=0a94727d6456bb7fe8d9f84cff11b4ce6d3a913f",
+  apiSearch: "https://api.github.com/users/Julzpeter?access_token=0a94727d6456bb7fe8d9f84cff11b4ce6d3a913f"
 };
