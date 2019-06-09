220fb6c304597ae6cd5ede7538d5e654fede1934
Juliet <chepngetichjuliet@gmail.com>
2019-06-09 19:25:06 +0300

    imports and adds content to the search.component.ts

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,36 +1,36 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient} from '@angular/common/http';
-import { RepoService} from '../repo.service';
-import { Repo} from '../repo';
+import { HttpClient } from '@angular/common/http';
+import { RepoService } from '../repo.service';
+import { Repo } from '../repo';
 
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css'],
-  providers:[RepoService]
+  providers: [RepoService]
 })
 export class SearchComponent implements OnInit {
 
   repo: Repo;
   result: any;
-  repoArray:any;
+  repoArray: any;
   link: string;
   searchValue: string;
-  
-  getRepos(reposearch){
 
-    
+  getRepos(reposearch) {
+
+
 
     this.link = "https://api.github.com/users/" + reposearch.value + '/repos' + '?acess_token0a94727d6456bb7fe8d9f84cff11b4ce6d3a913f';
-    this.http.get(this.link).subscribe((item)=>{
+    this.http.get(this.link).subscribe((item) => {
       this.repoArray = item;
       console.log(this.result);
 
     });
 
-    }
-  
+  }
+
 
 
   constructor(private http: HttpClient, private repoService: RepoService) { }
