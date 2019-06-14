b69e9c9e881100226dcd18862b07b2eae01d1b7d
Juliet <chepngetichjuliet@gmail.com>
2019-06-14 11:02:11 +0300

    adds the fifth argument in repo.ts

--- src/app/repo.ts
+++ src/app/repo.ts
@@ -1,3 +1,3 @@
 export class Repo {
-    constructor(public name: string, public id: number, public html_url, public description: string) { }
+    constructor(public name: string, public id: number, public html_url, public description: string,public created: Date) { }
 }
