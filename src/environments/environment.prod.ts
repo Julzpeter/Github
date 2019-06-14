e22565b77b190d795af0a902c41c2124da7171da
Juliet <chepngetichjuliet@gmail.com>
2019-06-14 12:34:35 +0300

    Auto-generated commit

--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  apiGithub: "https://api.github.com/users/Julzpeter?access_token=0a94727d6456bb7fe8d9f84cff11b4ce6d3a913f",
-  apiSearch: "https://api.github.com/users/Julzpeter?access_token=0a94727d6456bb7fe8d9f84cff11b4ce6d3a913f"
-};
